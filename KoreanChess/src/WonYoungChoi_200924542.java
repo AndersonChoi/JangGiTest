5a62a7b4a8f0165429d160ec2d5a12d96e248452
HackerAnderson <choco4235@pusan.ac.kr>
2016-04-08 14:40:45 +0900

    testJangGi = new KoreanChess();
    			생성자 없애고 각 테트마다 만듬

--- KoreanChess/src/WonYoungChoi_200924542.java
+++ KoreanChess/src/WonYoungChoi_200924542.java
@@ -9,13 +9,8 @@ import KoreanChessGame.Unit;
 
 public class WonYoungChoi_200924542 {
 
-	KoreanChess testJangGi = new KoreanChess();
+	KoreanChess testJangGi;
 	// 나는 먼저 success 부터한다 success는 성공한거 캡쳐도아니고 fail도 아니고 잘이동햇는지에 대한 정보를 담는다.
-
-	@Before
-	public void setUp() {
-		testJangGi = new KoreanChess();
-	}
 
 	@After
 	public void tearDown() {
@@ -30,6 +25,7 @@ public class WonYoungChoi_200924542 {
 	 */
 	@Test
 	public void testValidFirstPutBCHA() {
+		testJangGi = new KoreanChess();
 		PutResult result = testJangGi.putUnit(0, 0, Unit.BCHA, 0, 1);
 		assertEquals(PutResult.SUCCESS, result);
 		assertEquals(Unit.BCHA, testJangGi.getUnit(0, 1));
@@ -44,6 +40,7 @@ public class WonYoungChoi_200924542 {
 	 */
 	@Test
 	public void testValidFirstPutBSANG() {
+		testJangGi = new KoreanChess();
 		int[] beforeXY = {1,0};
 		int[] afterXY = {3,3};
 		PutResult result = testJangGi.putUnit(beforeXY[0], beforeXY[1], Unit.BSANG, afterXY[0], afterXY[1]);
@@ -59,6 +56,7 @@ public class WonYoungChoi_200924542 {
 	 */
 	@Test
 	public void testValidFirstPutBMA_01() {
+		testJangGi = new KoreanChess();
 		int[] beforeXY = {2,0};
 		int[] afterXY = {3,2};
 		PutResult result = testJangGi.putUnit(beforeXY[0], beforeXY[1], Unit.BMA, afterXY[0], afterXY[1]);
@@ -75,6 +73,7 @@ public class WonYoungChoi_200924542 {
 	 */
 	@Test
 	public void testValidFirstPutBMA_02() {
+		testJangGi = new KoreanChess();
 		int[] beforeXY = {3,2};
 		int[] afterXY = {4,0};
 		testJangGi.putUnit(2,0, Unit.BMA, 3,2);
@@ -92,6 +91,7 @@ public class WonYoungChoi_200924542 {
 	 */
 	@Test
 	public void testValidFirstPutBSA_01() {
+		testJangGi = new KoreanChess();
 		int[] beforeXY = {3,0};
 		int[] afterXY = {4,0};
 		PutResult result = testJangGi.putUnit(beforeXY[0], beforeXY[1], Unit.BSA, afterXY[0], afterXY[1]);
@@ -107,6 +107,7 @@ public class WonYoungChoi_200924542 {
 	 */
 	@Test
 	public void testValidFirstPutBSA_02() {
+		testJangGi = new KoreanChess();
 		int[] beforeXY = {3,0};
 		int[] afterXY = {3,1};
 		PutResult result = testJangGi.putUnit(beforeXY[0], beforeXY[1], Unit.BSA, afterXY[0], afterXY[1]);
@@ -167,6 +168,7 @@ public class WonYoungChoi_200924542 {
 	public void testValidMoveBJANG() {
 		// 1 - putUnit BJANG (4,1) -> (4,0) 
 		{
+			testJangGi = new KoreanChess();
 			int[] beforeXY = {4,1};
 			int[] afterXY = {4,0};
 			PutResult result = testJangGi.putUnit(beforeXY[0], beforeXY[1], Unit.BJANG, afterXY[0], afterXY[1]);
@@ -175,6 +177,7 @@ public class WonYoungChoi_200924542 {
 		}
 		// 2 - putUnit BJANG (4,1) -> (3,1) 
 		{
+			testJangGi = new KoreanChess();
 			int[] beforeXY = {4,1};
 			int[] afterXY = {3,1};
 			PutResult result = testJangGi.putUnit(beforeXY[0], beforeXY[1], Unit.BJANG, afterXY[0], afterXY[1]);
@@ -183,6 +186,7 @@ public class WonYoungChoi_200924542 {
 		}
 		// 3 - putUnit BJANG (4,1) -> (5,1) 
 		{
+			testJangGi = new KoreanChess();
 			int[] beforeXY = {4,1};
 			int[] afterXY = {5,1};
 			PutResult result = testJangGi.putUnit(beforeXY[0], beforeXY[1], Unit.BJANG, afterXY[0], afterXY[1]);
@@ -191,6 +195,7 @@ public class WonYoungChoi_200924542 {
 		}
 		// 4 - putUnit BJANG (4,1) -> (3,2) 
 		{
+			testJangGi = new KoreanChess();
 			int[] beforeXY = {4,1};
 			int[] afterXY = {3,2};
 			PutResult result = testJangGi.putUnit(beforeXY[0], beforeXY[1], Unit.BJANG, afterXY[0], afterXY[1]);
@@ -199,6 +204,7 @@ public class WonYoungChoi_200924542 {
 		}
 		// 5 - putUnit BJANG (4,1) -> (4,2) 
 		{
+			testJangGi = new KoreanChess();
 				int[] beforeXY = {4,1};
 				int[] afterXY = {4,2};
 				PutResult result = testJangGi.putUnit(beforeXY[0], beforeXY[1], Unit.BJANG, afterXY[0], afterXY[1]);
@@ -207,6 +213,7 @@ public class WonYoungChoi_200924542 {
 		}
 		// 6 - putUnit BJANG (4,1) -> (5,2)
 		{
+			testJangGi = new KoreanChess();
 			int[] beforeXY = {4,1};
 			int[] afterXY = {5,2};
 			PutResult result = testJangGi.putUnit(beforeXY[0], beforeXY[1], Unit.BJANG, afterXY[0], afterXY[1]);
@@ -215,6 +222,7 @@ public class WonYoungChoi_200924542 {
 		}
 		// 7 - putUnit BJANG (4,1) -> (5,1) 
 		{
+			testJangGi = new KoreanChess();
 			int[] beforeXY = {4,1};
 			int[] afterXY = {5,1};
 			PutResult result = testJangGi.putUnit(beforeXY[0], beforeXY[1], Unit.BJANG, afterXY[0], afterXY[1]);
@@ -223,6 +231,7 @@ public class WonYoungChoi_200924542 {
 		}
 		// 8 - putUnit BJANG (4,1) -> (3,0) 
 		{
+			testJangGi = new KoreanChess();
 			int[] beforeXY = {4,1};
 			int[] afterXY = {3,0};
 			testJangGi.putUnit(3,0, Unit.BSA, 3,1);
@@ -233,6 +242,7 @@ public class WonYoungChoi_200924542 {
 		}
 		// 9 - putUnit BJANG (4,1) -> (5,0) 
 		{
+			testJangGi = new KoreanChess();
 			int[] beforeXY = {4,1};
 			int[] afterXY = {5,0};
 			testJangGi.putUnit(5,0, Unit.BSA, 5,1);
@@ -303,6 +313,7 @@ public class WonYoungChoi_200924542 {
 	public void testValidMoveBPHO() {
 		// 1 - putUnit BPHO (1,2) -> (4,2) 
 		{
+			testJangGi = new KoreanChess();
 			int[] beforeXY = {1,2};
 			int[] afterXY = {4,2};
 			testJangGi.putUnit(2,0, Unit.BMA, 3,2);
@@ -313,6 +324,7 @@ public class WonYoungChoi_200924542 {
 		}
 		// 2 - putUnit BPHO (1,2) -> (1,4) 
 		{
+			testJangGi = new KoreanChess();
 			int[] beforeXY = {1,2};
 			int[] afterXY = {1,4};
 			testJangGi.putUnit(0,3, Unit.BJOL, 1,3);
@@ -323,6 +335,7 @@ public class WonYoungChoi_200924542 {
 		}
 		// 3 - putUnit BPHO (1,2) -> (4,2) 
 		{
+			testJangGi = new KoreanChess();
 			int[] beforeXY = {1,2};
 			int[] afterXY = {4,2};
 			testJangGi.putUnit(4,1, Unit.BJANG, 3,2);
@@ -333,6 +346,7 @@ public class WonYoungChoi_200924542 {
 		}
 		// 4 - Input : putUnit BPHO (1,2) -> (4,2) 
 		{
+			testJangGi = new KoreanChess();
 			int[] beforeXY = {1,2};
 			int[] afterXY = {4,2};
 			testJangGi.putUnit(3,0, Unit.BSA, 3,1);
@@ -345,6 +359,7 @@ public class WonYoungChoi_200924542 {
 		}
 		// 5 - Input : putUnit BPHO (4,2) -> (4,4)
 		{
+			testJangGi = new KoreanChess();
 			int[] beforeXY = {4,2};
 			int[] afterXY = {4,4};
 			testJangGi.putUnit(2,0, Unit.BMA, 3,2);
@@ -361,6 +376,7 @@ public class WonYoungChoi_200924542 {
 		}
 		// 6 - putUnit BPHO (4,2) -> (0,2)
 		{
+			testJangGi = new KoreanChess();
 			int[] beforeXY = {4,2};
 			int[] afterXY = {0,2};
 			testJangGi.putUnit(2,0, Unit.BMA, 3,2);
@@ -379,6 +395,7 @@ public class WonYoungChoi_200924542 {
 		}
 		// 7 - putUnit BPHO (5,2) -> (3,0)
 		{
+			testJangGi = new KoreanChess();
 			int[] beforeXY = {5,2};
 			int[] afterXY = {3,0};
 			testJangGi.putUnit(4,1, Unit.BJANG, 3,2);
@@ -393,6 +410,7 @@ public class WonYoungChoi_200924542 {
 		}
 		// 8 - putUnit BPHO (1,2) -> (1,7)
 		{
+			testJangGi = new KoreanChess();
 			int[] beforeXY = {1,2};
 			int[] afterXY = {1,7};
 			testJangGi.putUnit(0,0, Unit.BCHA, 0,1);
@@ -407,6 +425,7 @@ public class WonYoungChoi_200924542 {
 		}
 		// 9 - putUnit BPHO (1,2) -> (1,5)
 		{
+			testJangGi = new KoreanChess();
 			int[] beforeXY = {1,2};
 			int[] afterXY = {1,5};
 			testJangGi.putUnit(0,0, Unit.BCHA, 0,1);
@@ -421,6 +440,7 @@ public class WonYoungChoi_200924542 {
 		}
 		// 10 - putUnit BPHO (1,2) -> (1,5)
 		{
+			testJangGi = new KoreanChess();
 			int[] beforeXY = {1,2};
 			int[] afterXY = {1,5};
 			testJangGi.putUnit(0,0, Unit.BCHA, 0,1);
